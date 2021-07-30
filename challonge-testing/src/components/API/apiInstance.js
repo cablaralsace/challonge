8362f4e84f72dbe516026f766a3595ac460d9743
Alsace Cablar <cablaralsace@gmail.com>
2021-07-30 20:28:44 +0800

    alsace: remove key

--- challonge-testing/src/components/API/apiInstance.js
+++ challonge-testing/src/components/API/apiInstance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const apiInstance = axios.create({
     baseURL: 'https://api.challonge.com/v2',
-    timeout: 2000,
+    timeout: 5000,
     headers: {
-      'Authorization': 'Q4msiCUTwyPmXzut3wdspTnfAStFxPqcuzSS4U0r',
+      'Authorization': '',
       'Authorization-Type': 'v1',
       'Content-Type': 'application/vnd.api+json',
       'Accept': 'application/json'
